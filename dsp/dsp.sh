461f7421d5953e1d7531a60c7e2099da858c9abb
javadbayat <31417320+javadbayat@users.noreply.github.com>
2023-09-26 01:41:40 +0330

    Add files via upload

--- dsp/dsp.sh
+++ dsp/dsp.sh
@@ -1,2 +1,9 @@
-[ "$1" = "x" ] && cnt && read
+if [ "$1" = "q1" ]
+then
+bind -x '"\C-x": cnt && source tor-dsp q2'
+elif [ "$1" = "q2" ]
+then
+bind -x '"\C-x": tor-dsp'
+else
 tor SocksPort 9050 SocksPort $(ifconfig | grep inet | tail -1 | awk '{print $2}'):9050
+fi
