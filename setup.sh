d09849333cb454d3ba960c94bfc1d5d56ee1cc3d
Javad Bayat <javad78905@gmail.com>
2024-09-29 18:29:17 +0330

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ pkg upgrade -y
 pkg install tor golang git -y
 git clone https://git.torproject.org/pluggable-transports/snowflake.git
 clear
-echo "You no longer need your VPN! Please turn it off and press Enter to continue installation."
+echo -en "\e[1;34m You no longer need your VPN! Please turn it off and press Enter to continue installation. \e[0m"
 read
 clear
 cd snowflake/client
@@ -27,5 +27,5 @@ rm $PREFIX/etc/tor/torrc
 curl https://javadbayat.github.io/TorArticles/torrc -o $PREFIX/etc/tor/torrc
 curl https://javadbayat.github.io/TorArticles/tor-shortcut.sh >> ~/.bashrc
 clear
-echo "The final step is to set the Tor Network proxy in your Telegram! So please select Telegram app on the pop-up box that appears, then after Telegram app launches, you must confirm the creation of the new proxy."
+echo -e "\e[1;35m The final step is to set the Tor Network proxy in your Telegram! So please select Telegram app on the pop-up box that appears, then after Telegram app launches, you must confirm the creation of the new proxy. \e[0m"
 termux-open-url 'https://t.me/socks?server=127.0.0.1&port=9050&user=admin&pass=admin'
